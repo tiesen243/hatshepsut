20f177ae4d45ab5e1e9c31c4faa777faafafd973
tiesen243 <ttien56906@gmail.com>
2025-08-24 01:05:28 +0700

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
+import type { Plugin } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import liveReload from 'vite-plugin-live-reload'
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     liveReload(['app/**/*.php', 'resources/views/**/*.tpl.php']),
     tailwindcss(),
+    cleanPublicFolder(),
   ],
 
   server: {
@@ -22,22 +24,57 @@ export default defineConfig({
   },
 
   build: {
-    outDir: 'public/assets',
+    outDir: 'public',
     manifest: true,
     copyPublicDir: false,
+    emptyOutDir: false,
     rollupOptions: {
       input: getInputs(),
       output: {
         assetFileNames(chunkInfo) {
           const fileName = chunkInfo.originalFileNames.at(0) ?? ''
-          if (fileName.endsWith('.css')) return 'css/[name].css'
+          if (fileName.endsWith('.css')) return 'assets/css/[name].css'
           return 'assets/[name].[extname]'
         },
-        entryFileNames: 'js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
 })
+
+function cleanPublicFolder(): Plugin {
+  return {
+    name: 'clean-public-folder',
+    apply: 'build',
+    buildStart() {
+      const publicDir = path.resolve(__dirname, 'public')
+      const excludeDirs = [
+        path.join('assets', 'images'),
+        path.join('assets', 'fonts'),
+      ].map((f) => path.resolve(publicDir, f))
+
+      const targets = [
+        path.resolve(publicDir, 'assets'),
+        path.resolve(publicDir, '.vite'),
+      ]
+
+      for (const target of targets) {
+        if (fs.existsSync(target)) {
+          if (excludeDirs.some((ex) => target === ex)) continue
+
+          if (path.basename(target) === 'assets') {
+            for (const entry of fs.readdirSync(target)) {
+              const entryPath = path.join(target, entry)
+              if (!excludeDirs.includes(entryPath))
+                fs.rmSync(entryPath, { recursive: true, force: true })
+            }
+          } else fs.rmSync(target, { recursive: true, force: true })
+        }
+      }
+    },
+  }
+}
 
 function getInputs() {
   const inputs: Record<string, string> = {}
