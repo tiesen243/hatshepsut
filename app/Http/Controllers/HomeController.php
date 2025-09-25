75a7430a38713f7b53b4a487c45f787c92fe42cd
tiesen243 <ttien56906@gmail.com>
2025-09-25 19:09:03 +0700

    chore: minor changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,21 @@ class HomeController
 {
   public function index(): Response
   {
-    return Response::view('routes.home.index');
+    return Response::json([
+      'message' => 'Welcome to the HomeController index method!',
+      'routes' => [
+        '/api/health' => 'Health Check',
+        '/api/posts/store' => 'PostController@store',
+        '/api/posts/:id/delete' => 'PostController@delete',
+        '/api/protected' => 'Protected API Route (auth middleware)',
+
+        '/' => 'HomeController@index',
+        '/posts' => 'PostController@index',
+        '/posts/create' => 'PostController@create',
+        '/posts/:id' => 'PostController@show',
+
+        '/*' => '404 Not Found',
+      ],
+    ]);
   }
 }
--- resources/views/routes/home/index.tpl.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-  <main class="container flex min-h-dvh flex-col gap-4 py-4">
-    <h1 class="mb-4 text-3xl font-bold">Home</h1>
-    <p class="text-lg">Welcome to the home page!</p>
-  </main>
-@endsection
